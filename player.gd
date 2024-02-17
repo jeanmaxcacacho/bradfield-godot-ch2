cc43a85eb40812e8f1cee8b0c0b16951ddc79ce6
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-02-17 15:56:28 +0800

    direction based animations, signals, start, and die

--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+# to be emitted after the appropriate collisions
+signal pickup
+signal hurt
+
 @export var speed = 350
 var velocity = Vector2.ZERO # speed and direction?
 var screensize = Vector2(480, 720) # I think this is to define bounds of where player can move
@@ -17,3 +21,20 @@ func _process(delta):
 	position += velocity * speed * delta # when the fuck was this variable initialized???????
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
+	
+	# deciding on which running animation based on boolean logic
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = "run"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+
+func start():
+	set_process(true) # initialize the start of the _process(delta) calls
+	position = screensize/2
+	$AnimatedSprite2D.animation = "idle"
+
+func die():
+	$AnimatedSprite2D.animation = "hurt"
+	set_process(false) # stop the _process(delta) calls
