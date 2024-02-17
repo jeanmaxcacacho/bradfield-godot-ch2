6fc8c9a2c5753604693963dcb632d27050214648
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-02-18 00:18:44 +0800

    weird fucking UI workaround and _on_timer_timeout()

--- hud.gd
+++ hud.gd
@@ -3,14 +3,18 @@ extends CanvasLayer
 signal start_game
 
 func update_score(value):
-	$MarginContainer/Score.text = str(value)
+	$scoreAndTime/scoreContainer/Score.text = str(value)
 	
 	
 func update_timer(value):
-	$MarginContainer/Time.text = str(value)
+	$scoreAndTime/timeContainer/Time.text = str(value)
 
 
 func show_message(text):
 	$Message.text = text
 	$Message.show()
 	$Timer.start()
+
+
+func _on_timer_timeout():
+	$Message.hide()
