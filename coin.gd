ef2319bb72ef315ca5d54b8a3008c8218c80bfd4
jeanmaximuscacacho <jean.cacacho@student.ateneo.edu>
2024-02-18 11:18:42 +0800

    fully functioning cack2s

--- coin.gd
+++ coin.gd
@@ -4,3 +4,9 @@ var screensize = Vector2.ZERO # idk what the fuck this means
 
 func pickup():
 	queue_free() # remove the node
+
+
+# make cactus and coin not overlap; if has cactus regenerate position
+func _on_area_entered(area):
+	if area.is_in_group("obstacles"):
+		position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
